955d75f604b1a8587e2ab180c483ada238969cb9
Papop-san <008khun159@gmail.com>
2024-01-04 15:04:12 +0700

    flutter update

--- week3_1/lib/form.dart
+++ week3_1/lib/form.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
-import 'shopping.dart';
+import 'shopping.dart'; // Add this line if 'fromShopping()' is defined in 'shopping.dart'.
 
-class MyForm extends StatelessWidget {
+class MyForm extends StatefulWidget {
   const MyForm({Key? key}) : super(key: key);
+
+  @override
+  _MyFormState createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  var _productName;
+  var _userName;
+
+  final _userNameController = TextEditingController();
+  final _productNameController = TextEditingController();
+
+  void initState() {
+    super.initState();
+    _userNameController.addListener(_updateText);
+    _productNameController.addListener(_updateText);
+  }
+
+  void _updateText() {
+    setState(() {
+      _productName = _productNameController.text;
+      _userName = _userNameController.text;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +35,37 @@ class MyForm extends StatelessWidget {
         title: Text("Test Form Input"),
       ),
       body: Container(
-        child: Column(
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
           children: [
-            TextField(),
+            TextField(
+              controller: _userNameController,
+              decoration: InputDecoration(
+                hintText: "Username",
+                icon: Icon(Icons.verified_user_outlined),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+
+            TextFormField(
+              controller: _productNameController,
+              decoration: InputDecoration(
+                labelText: "Product Name",
+                icon: Icon(Icons.verified_user_outlined),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
             MyBtn(context),
-            MyBtn(context),
-            MyBtn(context),
+            Text(
+              "Username is : $_userName",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "The product name is : $_productName", // Corrected the typo here
+              style: TextStyle(fontSize: 20),
+            )
           ],
         ),
       ),
@@ -33,7 +82,6 @@ class MyForm extends StatelessWidget {
             Icon(Icons.add_shopping_cart_outlined),
           ],
         ),
-        style: ElevatedButton.styleFrom(fixedSize:  Size(300, 200)), // Fix the style property
         onPressed: () {
           Navigator.push(
             context,
@@ -44,6 +92,17 @@ class MyForm extends StatelessWidget {
             ),
           );
         },
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(20.0),
+          fixedSize: Size(300, 80),
+          textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          primary: Colors.blueAccent,
+          onPrimary: Colors.white,
+          elevation: 15,
+          shadowColor: Colors.blueAccent,
+          alignment: Alignment.topLeft,
+          shape: StadiumBorder(),
+        ),
       ),
     );
   }
