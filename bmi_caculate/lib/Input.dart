313f2e218727ca10c78dc2cbd01bc0145818d769
Papop-san <008khun159@gmail.com>
2024-01-18 20:54:12 +0700

    Bmi  Final

--- bmi_caculate/lib/Input.dart
+++ bmi_caculate/lib/Input.dart
@@ -16,37 +16,32 @@ class _InputfromState extends State<Inputfrom> {
   late double _age;
   late double _height;
   late double _weight;
-  
+
   final _ageController = TextEditingController();
-  final _weightController =  TextEditingController();
+  final _weightController = TextEditingController();
   final _heightController = TextEditingController();
 
-
-  void initState(){
+  void initState() {
     super.initState();
     _ageController.addListener(_updateText);
     _weightController.addListener(_updateText);
     _heightController.addListener(_updateText);
   }
 
-  void _updateText(){
-    _age = double.tryParse(_ageController.text)??0;
-    _height= double.tryParse(_heightController.text)??0;
-    _weight = double.tryParse(_weightController.text)??0;
-  
+  void _updateText() {
+    _age = double.tryParse(_ageController.text) ?? 0;
+    _height = double.tryParse(_heightController.text) ?? 0;
+    _weight = double.tryParse(_weightController.text) ?? 0;
   }
 
-
-
-
-
- 
- 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Caculate BMI",style: TextStyle(color: Colors.white),),
+        title: Text(
+          "Caculate BMI",
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Color(0xFF43766C),
       ),
       body: Container(
@@ -58,112 +53,110 @@ class _InputfromState extends State<Inputfrom> {
               padding: const EdgeInsets.only(bottom: 10.0),
               child: DropdownButton<String>(
                 value: selectGender,
-                onChanged: (String? newValue){
+                onChanged: (String? newValue) {
                   setState(() {
                     selectGender = newValue!;
                   });
                 },
-                items: <String>['Male' ,'Female'].map((String value){
-                    return DropdownMenuItem<String>(
-                      value:value ,
-                      child: Text(value),
-                    );
+                items: <String>['Male', 'Female'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
                 }).toList(),
               ),
             ),
             Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  selectGender == "Male"
-                      ? 'assets/images/men.png'
-                      : 'assets/images/women.png',
-                  width: 300,
-                  height: 300,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    selectGender == "Male"
+                        ? 'assets/images/men.png'
+                        : 'assets/images/women.png',
+                    width: 300,
+                    height: 300,
+                  ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
             //Age
             Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: TextFormField(
-                  controller: _ageController,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                     FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-                  ],
-                  decoration: InputDecoration(
-                    labelText: 'Age',
-                    prefixIcon: Icon(Icons.watch_later_outlined),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF5F8670)),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF5F8670)),
-                    ),
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: TextFormField(
+                controller: _ageController,
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                ],
+                decoration: InputDecoration(
+                  labelText: 'Age',
+                  prefixIcon: Icon(Icons.watch_later_outlined),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF5F8670)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF5F8670)),
                   ),
                 ),
               ),
+            ),
 
             //Weight
             Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: TextFormField(
-                  controller: _weightController,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                     FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-                  ],
-                  decoration: InputDecoration(
-                    labelText: 'Weight',
-                    prefixIcon: Icon(Icons.monitor_weight_outlined),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF5F8670)),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF5F8670)),
-                    ),
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: TextFormField(
+                controller: _weightController,
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                ],
+                decoration: InputDecoration(
+                  labelText: 'Weight',
+                  prefixIcon: Icon(Icons.monitor_weight_outlined),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF5F8670)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF5F8670)),
                   ),
                 ),
               ),
+            ),
 
             //Height
             Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: TextFormField(
-                  controller: _heightController,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                     FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-                  ],
-                  decoration: InputDecoration(
-                    labelText: 'Height',
-                    prefixIcon: Icon(Icons.height),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF5F8670)),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF5F8670)),
-                    ),
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: TextFormField(
+                controller: _heightController,
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                ],
+                decoration: InputDecoration(
+                  labelText: 'Height',
+                  prefixIcon: Icon(Icons.height),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF5F8670)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF5F8670)),
                   ),
                 ),
               ),
+            ),
 
-
-
-           //Button
+            //Button
             newMethod(context)
           ],
         ),
@@ -171,58 +164,50 @@ class _InputfromState extends State<Inputfrom> {
     );
   }
 
-
-
-
-
   Center newMethod(BuildContext context) {
     return Center(
-    child: ElevatedButton(
-      onPressed: () {  
-        Navigator.push(context,
-        
-          MaterialPageRoute(builder:(context){
-           if(selectGender == "Male"){
-            return Showresult(
-              ageResult:_age,
-              heightResult:_height,
-              weightResult:_weight,
-              genderResult: selectGender,
-            );
-          }
-          else{
-            return Showresult(
-              ageResult:_age,
-              heightResult:_height,
-              weightResult:_weight,
-              genderResult: selectGender,
-            );
-          }
-          })
-        );
-      },
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.calculate_outlined),
-          Text(
-            "Calculate",
-            textAlign: TextAlign.center, // Optional
-          )
-        ],
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            if (selectGender == "Male") {
+              return Showresult(
+                ageResult: _age,
+                heightResult: _height,
+                weightResult: _weight,
+                genderResult: selectGender,
+              );
+            } else {
+              return Showresult(
+                ageResult: _age,
+                heightResult: _height,
+                weightResult: _weight,
+                genderResult: selectGender,
+              );
+            }
+          }));
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.calculate_outlined),
+            Text(
+              "Calculate",
+              textAlign: TextAlign.center, // Optional
+            )
+          ],
+        ),
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(20.0),
+          fixedSize: Size(200, 70),
+          textStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          primary: Color(0xFF43766C),
+          onPrimary: Colors.white,
+          elevation: 15,
+          shadowColor: Color(0xFF43766C),
+          alignment: Alignment.center, // Center button content vertically
+          shape: StadiumBorder(),
+        ),
       ),
-       style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.all(20.0),
-        fixedSize: Size(200, 70),
-        textStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-        primary: Color(0xFF43766C),
-        onPrimary: Colors.white,
-        elevation: 15,
-        shadowColor: Color(0xFF43766C),
-        alignment: Alignment.center, // Center button content vertically
-        shape: StadiumBorder(),
-      ),
-    ),
-  );
- }
+    );
+  }
 }
--- bmi_caculate/lib/show.dart
+++ bmi_caculate/lib/show.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-// ignore: must_be_immutable
 class Showresult extends StatelessWidget {
   final double ageResult;
   final double heightResult;
   final double weightResult;
-  String  genderResult;
+  final String genderResult;
 
   Showresult({
     Key? key,
@@ -15,17 +14,32 @@ class Showresult extends StatelessWidget {
     required this.genderResult,
   }) : super(key: key);
 
+  String getWeightLossMessage(double bmi) {
+    if (bmi < 18.5) {
+      return "You are underweight. Consider gaining weight.";
+    } else if (bmi >= 18.5 && bmi < 22.9) {
+      return "Your BMI is in a healthy range. Maintain your current weight.";
+    } else if (bmi >= 23 && bmi < 24.9) {
+      return "Your BMI indicates you are overweight. Consider losing weight.";
+    } else if (bmi >= 25 && bmi < 29.9) {
+      return "You are in the pre-obesity range. Consider losing weight.";
+    } else {
+      return "You are in the obesity range. Consider losing weight.";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    //BMI Caculate
-    double bmiCaculate = weightResult / ((heightResult / 100) * (heightResult / 100));
-    //BMR Caculate
-    double bmrCaculateMale = 66+(13.7 *weightResult)+(5*(heightResult/100))-(6.8*ageResult);
-    double bmrCaculateFemale = 665+(9.6 *weightResult)+(1.8*(heightResult/100))-(4.7*ageResult);
-    
-
-
+    double bmiCalculate =
+        weightResult / ((heightResult / 100) * (heightResult / 100));
+    double bmrCalculateMale = 66 +
+        (13.7 * weightResult) +
+        (5 * (heightResult / 100)) -
+        (6.8 * ageResult);
+    double bmrCalculateFemale = 665 +
+        (9.6 * weightResult) +
+        (1.8 * (heightResult / 100)) -
+        (4.7 * ageResult);
 
     return Scaffold(
       appBar: AppBar(
@@ -36,7 +50,8 @@ class Showresult extends StatelessWidget {
         backgroundColor: Color(0xFF43766C),
       ),
       body: Center(
-        child: ListView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -53,29 +68,78 @@ class Showresult extends StatelessWidget {
                 ),
               ],
             ),
-            // BMI Caculate
+
+            // BMI Calculate
+            ListTile(
+              contentPadding: EdgeInsets.only(bottom: 0.0),
+              title: Text(
+                "BMI: ${bmiCalculate.toStringAsFixed(2)}",
+                textAlign: TextAlign.center,
+              ),
+            ),
+
+            // BMR Calculate
             if (genderResult == "Male")
               ListTile(
-                leading: Icon(Icons.boy_outlined),
-                title: Text("BMI: ${bmiCaculate.toStringAsFixed(2)}"),
+                title: Text(
+                  "BMR: ${bmrCalculateMale.toStringAsFixed(2)}",
+                  textAlign: TextAlign.center,
+                ),
               ),
             if (genderResult == "Female")
               ListTile(
-                leading: Icon(Icons.girl_outlined),
-                title: Text("BMI: ${bmiCaculate.toStringAsFixed(2)}"),
+                title: Text(
+                  "BMR: ${bmrCalculateFemale.toStringAsFixed(2)}",
+                  textAlign: TextAlign.center,
+                ),
               ),
 
-            // BMR Caculate
-            if (genderResult == "Male")
-              ListTile(
-                leading: Icon(Icons.fire_hydrant_alt_outlined),
-                title: Text("BMR: ${bmrCaculateMale.toStringAsFixed(2)}"),
+            // Image Creat
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (bmiCalculate < 18.5)
+                  Image.asset(
+                    'assets/images/bmi-1.png',
+                    width: 200,
+                    height: 300,
+                  ),
+                if (bmiCalculate >= 18.5 && bmiCalculate < 22.9)
+                  Image.asset(
+                    'assets/images/bmi-2.png',
+                    width: 200,
+                    height: 300,
+                  ),
+                if (bmiCalculate >= 23 && bmiCalculate < 24.9)
+                  Image.asset(
+                    'assets/images/bmi-3.png',
+                    width: 200,
+                    height: 300,
+                  ),
+                if (bmiCalculate >= 25 && bmiCalculate < 29.9)
+                  Image.asset(
+                    'assets/images/bmi-4.png',
+                    width: 200,
+                    height: 300,
+                  ),
+                if (bmiCalculate >= 30)
+                  Image.asset(
+                    'assets/images/bmi-5.png',
+                    width: 200,
+                    height: 300,
+                  ),
+              ],
+            ),
+
+            // Weight Loss Message
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                getWeightLossMessage(bmiCalculate),
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
               ),
-            if (genderResult == "Female")
-              ListTile(
-                leading: Icon(Icons.fire_hydrant_alt_outlined),
-                title: Text("BMR: ${bmrCaculateFemale.toStringAsFixed(2)}"),
-              ),
+            ),
           ],
         ),
       ),
